4c25b1eb4a446d732c74e4edbc85ef4bd0c6b31a
gboriss <galinaborissenko@gmail.com>
2019-12-03 00:26:33 +0300

    done

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
 let tasks = [
-	// {
-	// 	name: 'помыть кота',
-	// 	completed: false,
-	// },
-	// {
-	// 	name: 'погладить кота',
-	// 	completed: true,
-	// },
-	// {
-	// 	name: 'посушить кота',
-	// 	completed: false,
-	// },
+	{
+		name: 'помыть кота',
+		completed: false,
+	},
+	{
+		name: 'погладить кота',
+		completed: false,
+	},
+	{
+		name: 'посушить кота',
+		completed: false,
+	},
 ]
 
 
 const render = function () {
-	let listNode = document.querySelector('ul')
+	let listNode = document.querySelector('.incomplete-tasks')
 	listNode.innerHTML = ''
 
 	for (let i = 0; i < tasks.length; i++) {
 
 		let taskNode = document.createElement('li')
-
 		let checkBox = document.createElement('input')
+
 		checkBox.type = 'checkbox'
 		checkBox.checked = tasks[i].completed
 
 		checkBox.addEventListener('click', e => {
-			tasks[i].completed = e.srcElement.checked
+			if(tasks[i].completed = e.srcElement.checked)
+			{ulDone.append(taskDone)
+			taskDone.innerHTML = tasks[i].name
+			}
+
+			console.log(taskNode)
 
 			render()
 		})
+ 
+		let taskDone = document.createElement('li')
+		let ulDone = document.querySelector('.ulDone')
+	
+		// checkBox.addEventListener('click', e => {
+			
+		// })
+
+
 
 		taskNode.innerHTML = tasks[i].name;
 		if (tasks[i].completed) taskNode.innerHTML += ' ✔'
 
-		taskNode.prepend(checkBox)
-		listNode.append(taskNode)
+		taskNode.prepend(checkBox) //add checkBox
+		listNode.append(taskNode) // add list
 	}
 }
 
@@ -47,13 +61,15 @@ let sum = function(a, b) {
 }
 
 
+
+// enter, add list
 document.querySelector('input').addEventListener('keydown', function (e) {
 	if (e.keyCode === 13) {
 
 		if (this.value.trim() === '') return
 
 		tasks.push({
-			name: this.value + Math.random() * 100,
+			name: this.value,
 			completed: false,
 		})
 
